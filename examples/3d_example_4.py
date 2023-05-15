6d292d19aca257593470a8430ede27179d619ad0
spaceeye <superspaceeye@gmail.com>
2023-05-15 17:40:19 +0300

    insanity

--- examples/3d_example_4.py
+++ examples/3d_example_4.py
@@ -32,22 +32,34 @@ def time_fn(): return t  # for pids
 
 
 g = 10
+drag = 0.99
 
-pursuer_v = g * 3
+pursuer_acc = g * 3
+
 target_v  = g
 
-pursuer = pn.HeadingVelocity3d(np.deg2rad(90), 0, np.array([0, 0, 0]), pursuer_v)
-lagged_pursuer = pn.HeadingVelocity3d(np.deg2rad(90), 0, np.array([0, 0, 0]), pursuer_v)
+# rotation and acceleration direction
+pursuer_rot = pn.HeadingVelocity3d(np.deg2rad(90), 0, np.array([0, 0, 0]), 0)
+# velocity and position
+pursuer_physic = pn.HeadingVelocity3d(0, 0, np.array([0, 20, 0]), 0)
+
+# lagged_pursuer_rot  = pn.HeadingVelocity3d(np.deg2rad(0), 0, np.array([0, 0, 0]), 0)
+# lagged_pursuer_phys = pn.HeadingVelocity3d(np.deg2rad(0), 0, np.array([0, 0, 0]), 0)
+lagged_pursuer_phys = pursuer_physic
+
 actual_target = pn.HeadingVelocity3d(np.deg2rad(0), 0, np.array([100, 100, 0]), target_v)
 lagged_target = pn.HeadingVelocity3d(np.deg2rad(0), 0, np.array([100, 100, 0]), target_v)
-dt = 1. / 20
+
+dt = 1. / 20 / 2
 N = 3
+
+update_actual_every = 1
 
 Kp, Kl, Kd = 1, 1, 1 # parameters of pid
 modif = 100. # allows pid finer control but makes it slower to respond
 limits = (np.deg2rad(-22.5) * modif, np.deg2rad(22.5) * modif) # simple_pid outputs only integers for some reason
 
-max_sim_time = 40  # maximum sim time
+max_sim_time = 20  # maximum sim time
 
 R_min = 5  # minimum distance between target and pursuer before terminating
 
@@ -82,65 +94,72 @@ log = {'pursuer': [], 'target': [], "lagged_target":[], "lagged_pursuer":[], "pu
 while True:
     # ========== Physics simulation ==========
     # simulating pursuer and target movement's
-    pursuer.pos += pursuer.vel * dt
-    pursuer.pos[1] -= g * dt
+    pursuer_physic.pos += pursuer_physic.vel * dt
+    pursuer_physic.pos[1] -= g * dt
+
+    if pursuer_physic.pos[1] < 0:
+        pursuer_physic.pos[1] = 0
 
     actual_target.pos += actual_target.vel * dt
-    # actual_target.yaw = np.cos(t)
-    actual_target.pitch = np.cos(t / 2)
+    actual_target.yaw = np.cos(t )
+    actual_target.pitch = np.cos(t / 4)
 
     # simulating angular inertia
-    pursuer.pitch += pitch_ar
-    pursuer.yaw += yaw_ar
+    pursuer_rot.pitch += pitch_ar
+    pursuer_rot.yaw += yaw_ar
 
     # ========== Homing loop ==========
-    if counter % 3 == 0:
+    if counter % update_actual_every == 0:
         lagged_target.pos = np.array(actual_target.pos)
         lagged_target.vel = np.array(actual_target.vel)
 
-        lagged_pursuer.pos = np.array(pursuer.pos)
-        lagged_pursuer.vel = np.array(pursuer.vel)
+        # lagged_pursuer_phys.pos = np.array(pursuer_physic.pos)
+        # lagged_pursuer_phys.vel = np.array(pursuer_physic.vel)
     else:
         lagged_target.pos += lagged_target.vel * dt
     counter+=1
 
-    ret = pn.ZEM_3d(lagged_pursuer, lagged_target, N=N)
+    ret = pn.ZEM_3d(lagged_pursuer_phys, lagged_target, N=N)
     nL = ret['nL']
     R = ret['R']
 
-    log["pursuer"].append(list(pursuer.pos))
+    log["pursuer"].append(list(pursuer_physic.pos))
     log['target'].append(list(actual_target.pos))
     log["lagged_target"].append(list(lagged_target.pos))
-    log["pursuer_vel"].append(np.sqrt(pursuer.vel.dot(pursuer.vel)))
-    log["lagged_pursuer"].append(list(lagged_pursuer.pos))
+    log["pursuer_vel"].append(np.sqrt(pursuer_physic.vel.dot(pursuer_physic.vel)))
+    # log["lagged_pursuer"].append(list(lagged_pursuer_phys.pos))
 
     t = t + dt
     if R <= R_min or t > max_sim_time:
         break
 
     # setting new targets and calculating gimbal
-    new_yaw, new_pitch = get_angles(nL*dt)
+    new_yaw, new_pitch = get_angles(nL)
     pitch_pid.set_starting(new_pitch); yaw_pid.set_starting(new_yaw)
-    pitch_gimbal, yaw_gimbal = pitch_pid(pursuer.pitch)/modif, yaw_pid(pursuer.yaw)/modif
+    pitch_gimbal, yaw_gimbal = pitch_pid(pursuer_rot.pitch) / modif, yaw_pid(pursuer_rot.yaw) / modif
 
-    pitch_ar_change = np.sin(pitch_gimbal) * pursuer_v * dt
-    yaw_ar_change   = np.sin(yaw_gimbal)   * pursuer_v * dt
+    pitch_ar_change = np.sin(pitch_gimbal) * pursuer_acc * dt
+    yaw_ar_change   = np.sin(yaw_gimbal)   * pursuer_acc * dt
 
-    lagged_pursuer.pitch += pitch_ar + pitch_ar_change
-    lagged_pursuer.yaw   += yaw_ar   + yaw_ar_change
+    # lagged_pursuer_rot.pitch += pitch_ar + pitch_ar_change
+    # lagged_pursuer_rot.yaw   += yaw_ar   + yaw_ar_change
 
-    pursuer.pitch = pi_clip(pursuer.pitch)
-    pursuer.yaw   = pi_clip(pursuer.yaw)
+    pursuer_rot.pitch = pi_clip(pursuer_rot.pitch)
+    pursuer_rot.yaw   = pi_clip(pursuer_rot.yaw)
 
-    lagged_pursuer.pitch = pi_clip(lagged_pursuer.pitch)
-    lagged_pursuer.yaw   = pi_clip(lagged_pursuer.yaw)
+    # lagged_pursuer_rot.pitch = pi_clip(lagged_pursuer_rot.pitch)
+    # lagged_pursuer_rot.yaw   = pi_clip(lagged_pursuer_rot.yaw)
+    #
+    # lagged_pursuer_phys.vel = (lagged_pursuer_phys.vel + lagged_pursuer_rot.vel * dt) * (1 - drag*dt)
     # ========== Post homing loop simulation ==========
 
     # simulating gimbal change of angular inertia
     pitch_ar += pitch_ar_change
     yaw_ar   += yaw_ar_change
 
-    pursuer.V = abs(np.cos(pitch_gimbal) * np.cos(yaw_gimbal)) * pursuer_v
+    pursuer_rot.V = pursuer_acc * abs(np.cos(pitch_gimbal) * np.cos(yaw_gimbal))
+
+    pursuer_physic.vel = (pursuer_physic.vel + pursuer_rot.vel * dt) * (1 - drag*dt)
 
     change_ticks += 1
 
@@ -149,15 +168,15 @@ distance = [sqrt((x1 - x2)**2 + (y1 - y2)**2) + (z1-z2)**2 for (x1, y1, z1), (x2
 
 print(min(distance))
 
-actual = np.array(log["pursuer"])
-lagged = np.array(log["lagged_pursuer"])
-
-print("pursuer_diff", abs(sum(actual - lagged)) / len(actual))
-
-actual = np.array(log["target"])
-lagged = np.array(log["lagged_target"])
-
-print("target_diff", abs(sum(actual - lagged)) / len(actual))
+# actual = np.array(log["pursuer"])
+# lagged = np.array(log["lagged_pursuer"])
+#
+# print("pursuer_diff", np.round(abs(sum(actual - lagged)) / len(actual)))
+#
+# actual = np.array(log["target"])
+# lagged = np.array(log["lagged_target"])
+#
+# print("target_diff", np.round(abs(sum(actual - lagged)) / len(actual)))
 
 fig = plt.figure(figsize=(12, 12))
 ax = fig.add_subplot(projection="3d")
@@ -176,4 +195,7 @@ ax.scatter(tx, tz, ty, c=range(len(distance)), cmap=matplotlib.colormaps["Reds"]
 # ax.scatter(lx, lz, ly, c=range(len(distance)), cmap=matplotlib.colormaps["Blues"])
 
 plt.show()
+
+# plt.plot(range(len(distance)), log["pursuer_vel"])
+# plt.show()
 
