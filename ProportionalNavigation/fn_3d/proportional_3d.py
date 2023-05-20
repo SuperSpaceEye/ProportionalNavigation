6b2be59b85b9c7426b9462052459d6dc2ce8a5c2
spaceeye <superspaceeye@gmail.com>
2023-05-21 01:23:58 +0300

    insanity

--- ProportionalNavigation/fn_3d/proportional_3d.py
+++ ProportionalNavigation/fn_3d/proportional_3d.py
@@ -2,26 +2,10 @@ import numpy as np
 
 from ProportionalNavigation.exceptions import InvalidProportionalGainError
 
-def rotate(ar, a):
-    x = ar[0]
-    # z = ar[2]
-    # ar[0] = np.cos(a) * x - np.sin(a) * z
-    # ar[2] = np.cos(a) * z + np.sin(a) * x
-    ar[0] = -x
-    # ar[2] = -z
-    return ar
-
 def ZEM_3d(pursuer, target, N=3):
     if N <= 0: raise InvalidProportionalGainError(N)
     dR = target.pos - pursuer.pos
     dV = target.vel - pursuer.vel
-
-    # rotated = False
-    # a = 180
-    # if dR[0] < 0:
-    #     rotated = True
-        # dR = rotate(dR, np.deg2rad(a))
-        # dV = rotate(dV, np.deg2rad(a))
 
     R = np.sqrt(dR.dot(dR))
     V = np.sqrt(dV.dot(dV))
@@ -35,8 +19,6 @@ def ZEM_3d(pursuer, target, N=3):
     ZEM_n = ZEM_i - ZEM_i.dot(LOS_u) * LOS_u  # ZEM normal to LOS
 
     nL = N * ZEM_n/(t_go * t_go)
-
-    # if rotated: nL = rotate(nL, -np.deg2rad(a))
 
     return {
         "nL": nL,
